5946ff3f435585152ed29af37fec17596ff41270
Simone Raimondi <simo@cleverit.ch>
2017-05-13 21:13:36 +0200

    Added differentiable object interface

---
+++ rad/diff_object.hpp
@@ -0,0 +1,29 @@
+//
+// Created by simon on 13.05.17.
+//
+
+#ifndef DRDEMO_DIFF_OBJECT_HPP
+#define DRDEMO_DIFF_OBJECT_HPP
+
+#include "rad.hpp"
+
+namespace drdemo {
+
+    /**
+     * Define differentiable object interface
+     */
+    class DiffObjectInterface {
+    public:
+        virtual ~DiffObjectInterface() {}
+
+        // Return a list of references to the differentiable variables in the object
+        virtual std::vector<Float const &> GetDiffVariables() const = 0;
+
+        // Update values of differentiable variables in the object, starting index tells the object
+        // from where to start to get the deltas
+        virtual void UpdateDiffVariables(std::vector<float> const &deltas, size_t starting_index) = 0;
+    };
+
+} // drdemo namespace
+
+#endif //DRDEMO_DIFF_OBJECT_HPP
