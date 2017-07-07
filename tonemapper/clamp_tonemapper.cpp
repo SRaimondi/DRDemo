501a2026db75602ddd9320c6ac2f6a95d31eaca3
Simone Raimondi <simo@cleverit.ch>
2017-07-07 12:23:10 +0100

    Added lodepng library and changed tonemapper to produce .png image

--- tonemapper/clamp_tonemapper.cpp
+++ tonemapper/clamp_tonemapper.cpp
@@ -3,35 +3,59 @@
 //
 
 #include "clamp_tonemapper.hpp"
-#include <fstream>
+#include "lodepng.hpp"
 
 namespace drdemo {
 
     void ClampTonemapper::Process(std::string const &file_name, Film const &film) const {
-        // Create output file
-        std::fstream file = std::fstream(file_name, std::fstream::out);
-        // Write ppm header
-        file << "P6\n";
-        file << film.Width() << " " << film.Height() << "\n";
-        file << "255\n";
+//        // Create output file
+//        std::fstream file = std::fstream(file_name, std::fstream::out);
+//        // Write ppm header
+//        file << "P6\n";
+//        file << film.Width() << " " << film.Height() << "\n";
+//        file << "255\n";
+//
+//        Spectrum final_spectrum;
+//        // Write colors to file
+//        for (int j = static_cast<int>(film.Height() - 1); j >= 0; j--) {
+//            for (unsigned i = 0; i < film.Width(); ++i) {
+//                final_spectrum = film.At(i, j);
+//
+//                // Clamp color and output to file
+//                unsigned char r = static_cast<unsigned char>(Clamp(final_spectrum.r.GetValue() * 255.f, 0.f, 255.f));
+//                unsigned char g = static_cast<unsigned char>(Clamp(final_spectrum.g.GetValue() * 255.f, 0.f, 255.f));
+//                unsigned char b = static_cast<unsigned char>(Clamp(final_spectrum.b.GetValue() * 255.f, 0.f, 255.f));
+//
+//                // Output spectrum to file
+//                file << r << g << b;
+//            }
+//        }
+//        // Close file
+//        file.close();
 
-        Spectrum final_spectrum;
-        // Write colors to file
-        for (int j = static_cast<int>(film.Height() - 1); j >= 0; j--) {
-            for (unsigned i = 0; i < film.Width(); ++i) {
-                final_spectrum = film.At(i, j);
+        // Create .png image
 
-                // Clamp color and output to file
-                unsigned char r = static_cast<unsigned char>(Clamp(final_spectrum.r.GetValue() * 255.f, 0.f, 255.f));
-                unsigned char g = static_cast<unsigned char>(Clamp(final_spectrum.g.GetValue() * 255.f, 0.f, 255.f));
-                unsigned char b = static_cast<unsigned char>(Clamp(final_spectrum.b.GetValue() * 255.f, 0.f, 255.f));
+        // Convert raster to unsigned char vector
+        std::vector<unsigned char> image;
+        image.reserve(film.Width() * film.Height() * 4);
 
-                // Output spectrum to file
-                file << r << g << b;
+        for (int j = static_cast<int>(film.Height()) - 1; j >= 0; j--) {
+            for (unsigned int i = 0; i < film.Width(); i++) {
+                Spectrum c = film.At(i, j); // this->operator()(i, j).Clamp(0, 1);
+                image.push_back(static_cast<unsigned char>(Clamp(c.r.GetValue() * 255.f, 0.f, 255.f)));
+                image.push_back(static_cast<unsigned char>(Clamp(c.g.GetValue() * 255.f, 0.f, 255.f)));
+                image.push_back(static_cast<unsigned char>(Clamp(c.b.GetValue() * 255.f, 0.f, 255.f)));
+                // Alpha
+                image.push_back(static_cast<unsigned char>(255.f));
             }
         }
-        // Close file
-        file.close();
+
+        // Encode the image
+        const unsigned error = lodepng::encode(file_name, image, film.Width(), film.Height());
+        // Check if there is an error
+        if (error) {
+            std::cerr << "Encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+        }
     }
 
 } // drdemo namespace
